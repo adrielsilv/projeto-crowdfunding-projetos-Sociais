01ab6ce245efbcf5e18c5e7aa0d9f162ce89f50e
Adriel Antonio <adrielantonio65@gmail.com>
2024-08-30 17:47:55 -0300

    header/js

--- public/main.js
+++ public/main.js
@@ -18,14 +18,24 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
+
 document.addEventListener('DOMContentLoaded', () => {
     window.addEventListener('scroll', () => {
         const header = document.querySelector('header');
-        if (window.scrollY > 50) {
+        if (window.scrollY > 150) {
             header.classList.add('scrolled');
         } else {
             header.classList.remove('scrolled');
         }
+    });
+
+    document.querySelectorAll('nav ul li a').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
     });
 
     document.querySelectorAll('nav ul li a').forEach(anchor => {
